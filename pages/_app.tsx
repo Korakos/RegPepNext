40b387239c86813920730007eb1a67c1c2d89eaf
Andres Molina <andres@losmolina.org>
2020-02-27 13:59:19 -0600

    changed site name

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,7 +21,7 @@ export default class MyApp extends App {
     return (
       <React.Fragment>
         <Head>
-          <title>My page</title>
+          <title>RegPep2020</title>
           <meta
             name="viewport"
             content="minimum-scale=1, initial-scale=1, width=device-width"
